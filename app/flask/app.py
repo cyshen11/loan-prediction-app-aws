229a1b9e88e14c600faf79a70e05884505284e6a
VincentCYS <vincent.chengyunsheng@gmail.com>
2025-04-26 21:38:58 +0800

    developed the prediction model

--- app/flask/app.py
+++ app/flask/app.py
@@ -7,80 +7,101 @@ import pandas as pd
 import random
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = str(random.random())
+app.config["SECRET_KEY"] = str(random.random())
+
 
 class LoanForm(FlaskForm):
-    district_names = ['Rokycany', 'Louny', 'Strakonice', 'Pribram', 'Hl.m. Praha',
-  'Brno - mesto', 'Melnik', 'Prachatice', 'Havlickuv Brod', 'Tachov',
-  'Bruntal', 'Zlin', 'Vyskov', 'Kromeriz', 'Pelhrimov',
-  'Usti nad Labem', 'Praha - vychod', 'Usti nad Orlici',
-  'Litomerice', 'Kutna Hora', 'Frydek - Mistek', 'Cheb', 'Opava',
-  'Cesky Krumlov', 'Olomouc', 'Zdar nad Sazavou', 'Ceske Budejovice',
-  'Prerov', 'Jicin', 'Semily', 'Benesov', 'Rakovnik', 'Pisek',
-  'Brno - venkov', 'Jihlava', 'Tabor', 'Karvina', 'Chrudim',
-  'Chomutov', 'Teplice', 'Domazlice', 'Karlovy Vary', 'Kolin',
-  'Plzen - jih', 'Kladno', 'Sumperk', 'Nachod', 'Liberec',
-  'Ostrava - mesto', 'Vsetin', 'Prostejov', 'Svitavy',
-  'Jindrichuv Hradec', 'Blansko', 'Pardubice', 'Hradec Kralove',
-  'Most', 'Praha - zapad', 'Trebic', 'Nymburk', 'Decin',
-  'Plzen - sever', 'Uherske Hradiste', 'Beroun',
-  'Rychnov nad Kneznou', 'Plzen - mesto', 'Novy Jicin', 'Trutnov',
-  'Breclav', 'Jesenik', 'Mlada Boleslav', 'Hodonin', 'Ceska Lipa',
-  'Znojmo', 'Sokolov', 'Klatovy']
-    district_names.sort()
-    
-    district = SelectField(
-        'District',
-        validators = [InputRequired()],
-        choices=district_names
+    loan_duration = IntegerField("Loan Duration (Months)", [InputRequired()])
+    loan_amount = IntegerField("Loan Amount", [InputRequired()])
+    loan_payments = IntegerField("Loan Payments", [InputRequired()])
+    account_creation_date = DateField("Account Creation Date", [InputRequired()])
+    loan_date = DateField("Loan Date", [InputRequired()])
+    account_frequency = SelectField(
+        "Account Frequency",
+        choices=[
+            ("POPLATEK MESICNE", "Monthly Issuance"),
+            ("POPLATEK TYDNE", "Weekly Issuance"),
+            ("POPLATEK PO OBRATU", "Issuance After Transaction"),
+        ],
     )
-    balance = IntegerField('Balance', [InputRequired()])
-    loan_amount = IntegerField('Loan Amount', [InputRequired()])
-    loan_duration = IntegerField('Loan Duration (Months)', [InputRequired()])
-    loan_payments = IntegerField('Loan Payments', [InputRequired()])
-    last_transaction_date = DateField('Last Transaction Date', [InputRequired()])
-    last_transaction_amount = IntegerField('Last Transaction Amount', [InputRequired()])
-    daily_transactions = IntegerField('Number of Daily Transactions on Average', [InputRequired()])
+    avg_order_amount = IntegerField(
+        "Average Payment Order Amount (based on past 12 months)", [InputRequired()]
+    )
+    avg_trans_amount = IntegerField(
+        "Average Transaction Amount (based on past 12 months)", [InputRequired()]
+    )
+    avg_trans_balance = IntegerField(
+        "Average Transaction Balance (based on past 12 months)", [InputRequired()]
+    )
+    n_trans = IntegerField(
+        "Number of Transactions since Inception", [InputRequired()]
+    )
+    card_type = SelectField(
+        "Card Type",
+        choices=[
+            ("classic", "Classic"),
+            ("gold", "Gold"),
+            ("junior", "Junior"),
+            (None, "Nil"),
+        ],
+    )
+    avg_salary = IntegerField("Monthly Salary", [InputRequired()])
+    gender = SelectField("Gender", choices=[("M", "Male"), ("F", "Female")])
+    age = IntegerField("Age", [InputRequired()])
+
 
 @app.route("/", methods=["POST", "GET"])
-def loan_prediction_app():    
+def loan_prediction_app():
     loan_status = ""
     show_modal = False
     form = LoanForm()
     if form.validate_on_submit():
         data = get_form_data(request)
-        loan_status=predict_loan_status(data)
-        show_modal=True
+        loan_status = predict_loan_status(data)
+        show_modal = True
         return render_template(
-            'index.html', 
-            form=form, 
-            loan_status=loan_status,
-            show_modal=show_modal
+            "index.html", form=form, loan_status=loan_status, show_modal=show_modal
         )
-        
+
     return render_template(
-        'index.html', 
-        form=form, 
-        loan_status=loan_status,
-        show_modal=show_modal
+        "index.html", form=form, loan_status=loan_status, show_modal=show_modal
     )
 
+
 def get_form_data(request):
-    return pd.DataFrame(data={
-        'district_name': [request.form.get('district')],
-        'trans_date_latest': [request.form.get('last_transaction_date')],
-        'loan_duration': [request.form.get('loan_duration')],
-        'loan_payments': [request.form.get('loan_payments')],
-        'loan_amount': [request.form.get('loan_amount')],
-        'trans_balance_latest': [request.form.get('balance')],
-        'median_daily_trans_count_p3m': [request.form.get('daily_transactions')],
-        'trans_amount_latest': [request.form.get('last_transaction_amount')]
-    })
+    data = {}
+    fields = [
+        'loan_duration',
+        'loan_amount',
+        'loan_payments',
+        'account_frequency',
+        'avg_order_amount',
+        'avg_trans_amount',
+        'avg_trans_balance',
+        'n_trans',
+        'card_type',
+        'avg_salary',
+        'gender',
+        'age',
+        'loan_date',
+        'account_creation_date'
+    ]
+    for f in fields:
+        data[f] = [request.form.get(f)]
+    df = pd.DataFrame(data=data)
+    df["days_between"] = pd.to_datetime(df["loan_date"]) - pd.to_datetime(
+        df["account_creation_date"]
+    )
+    df.drop(["account_creation_date", "loan_date"], axis=1, inplace=True)
+    print(df)
+    return df
+
 
 def predict_loan_status(data):
-    predictor = TabularPredictor.load('./static/model/final')
+    predictor = TabularPredictor.load("./static/model/final")
     prediction = predictor.predict(data).iloc[0]
-    
+    print(prediction)
+
     if prediction == "A":
         return "Loan will be paid off!"
     else:
